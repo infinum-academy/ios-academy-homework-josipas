75bae9197ee10e0bdccaba2d12bfa09fff5f378a
Josipa Supe <josipa2supe@gmail.com>
2019-07-11 14:04:38 +0200

    Empty storyboard.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -11,37 +11,24 @@ import UIKit
 final class LoginViewController: UIViewController {
 
     //MARK: - Outlets
-    
-    @IBOutlet private weak var activityIndicator : UIActivityIndicatorView!
-    @IBOutlet private weak var counterLabel: UILabel!
-    @IBOutlet private weak var touchCounterButton: UIButton!
+   
     
     //MARK: - Properties
     
-    private var numberOfClicks : Int = 0
+   
     
     //MARK: - Lifecycle methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //touchCounterButton.backgroundColor = UIColor.purple
-        //touchCounterButton.layer.cornerRadius = 10
+        
         
         // Do any additional setup after loading the view.
     }
     
     //MARK: - Actions
     
-    @IBAction func onButtonClick() {
-        numberOfClicks += 1
-        //print("Click!")
-        counterLabel.text = String(numberOfClicks)
-        
-        if activityIndicator.isAnimating {
-            activityIndicator.stopAnimating()
-        } else {
-            activityIndicator.startAnimating()
-        }
+   
     }
     /*
     // MARK: - Navigation
@@ -53,4 +40,4 @@ final class LoginViewController: UIViewController {
     }
     */
 
-}
+
