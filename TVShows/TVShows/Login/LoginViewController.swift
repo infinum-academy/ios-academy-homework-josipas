90b0dc5ca719754ab7c9c6ca0ff9e59b79f4a814
Josipa Supe <josipa2supe@gmail.com>
2019-07-15 23:08:00 +0200

    Problem with keyboard solved.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -15,6 +15,7 @@ final class LoginViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     //MARK: - Properties
     
    
@@ -29,7 +30,12 @@ final class LoginViewController: UIViewController {
         
         loginButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
     }
+    
     
     //MARK: - Actions
     
@@ -41,7 +47,23 @@ final class LoginViewController: UIViewController {
         }
     }
     
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            scrollView.contentInset = .zero
+        } else {
+            scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
     }
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
@@ -53,3 +75,4 @@ final class LoginViewController: UIViewController {
     */
 
 
+}
