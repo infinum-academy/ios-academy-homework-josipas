ff6d1db7088bc43b35d58e865d42cdf7c36e77c4
Josipa Supe <josipa2supe@gmail.com>
2019-07-16 10:59:25 +0200

    Added User.swift.

---
+++ TVShows/TVShows/Login/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  TVShows
+//
+//  Created by Infinum on 16/07/2019.
+//  Copyright © 2019 Infinum Academy. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let email: String
+    let type: String
+    let id: String
+    
+    enum CodingKeys: String, CodingKey {
+        case email
+        case type
+        case id = "_id"
+    }
+}
+
+struct LoginData: Codable {
+    let token: String
+}
