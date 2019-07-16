0dced610d4b274f2d3e1d742424dce1ee89afdfc
Josipa Supe <josipa2supe@gmail.com>
2019-07-16 10:17:28 +0200

    Merge improvs.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -59,22 +59,6 @@ final class LoginViewController: UIViewController {
         navigationController?.pushViewController(HomeViewController, animated: true)
     }
     
-
-   // override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-      //  if let mojViewController = segue.destination as? HomeViewController {
-        //    mojViewController.user = User(name: "Josipa")
-     //   }
-//
-//        if segue.destination is HomeViewController {
-//            let mojVi3weController = segue.destination as HomeViewController
-//        }
-        
-    }
-    
-    
- 
-    
     @objc func adjustForKeyboard(notification: Notification) {
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
         
@@ -87,20 +71,5 @@ final class LoginViewController: UIViewController {
             scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
         }
     }
-    
-    
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 
 }
