72f92bb4f83b9febd4e368c5b857222c449a1fa4
Josipa Supe <josipa2supe@gmail.com>
2019-07-18 17:56:02 +0200

    Add missing files.

--- TVShows/Pods/Target Support Files/Alamofire/Alamofire-umbrella.h
+++ TVShows/Pods/Target Support Files/Alamofire/Alamofire-umbrella.h
@@ -10,8 +10,7 @@
 #endif
 #endif
 
-#import "MBProgressHUD.h"
 
-FOUNDATION_EXPORT double MBProgressHUDVersionNumber;
-FOUNDATION_EXPORT const unsigned char MBProgressHUDVersionString[];
+FOUNDATION_EXPORT double AlamofireVersionNumber;
+FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
 
