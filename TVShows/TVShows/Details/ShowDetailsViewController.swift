e82ad12daa525486012e65ba55a6e2b6ddbfd9c7
Josipa Supe <josipa2supe@gmail.com>
2019-07-22 11:47:19 +0200

    Added new group Details.

---
+++ TVShows/TVShows/Details/ShowDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowDetailsViewController.swift
+//  TVShows
+//
+//  Created by Infinum on 22/07/2019.
+//  Copyright © 2019 Infinum Academy. All rights reserved.
+//
+
+import UIKit
+
+class ShowDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
